6219bf03d66c73477a3dbb95c492ed23dd2659ca
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2020-07-23 18:18:49 -0400

    fix bug with tests

--- tests/test_svf.py
+++ tests/test_svf.py
@@ -1,23 +1,23 @@
 import numpy as np
 import unittest
 
-from horizonpy.skyview import sky_view_factor, SVF_discretized, rotate_horizon
+from horizonpy.skyview import svf_helbig_2009, svf_steyn_1980, SVF_discretized, rotate_horizon
 
 from horizonpy.skyview import rotate_horizon
 
 
-class TestSkyView(unittest.TestCase):
+class TestSkyViewHelbig(unittest.TestCase):
     def test_all_sky_obscured(self):
         f = lambda x: x * 0 + 0
-        self.assertEqual(sky_view_factor(f, 1), 1.0)
+        self.assertEqual(svf_helbig_2009(f, 1), 1.0)
 
     def test_hemisphere_obscured(self):
         f = lambda x: np.array(np.mod(x, 360) < 180, dtype='int32') * 90
-        self.assertEqual(sky_view_factor(f, 1), 0.5)
+        self.assertEqual(svf_helbig_2009(f, 1), 0.5)
 
     def test_all_sky_open(self):
         f = lambda x: x * 0 + 90
-        self.assertEqual(sky_view_factor(f, 1), 0.0)
+        self.assertEqual(svf_helbig_2009(f, 1), 0.0)
 
 class TestDiscretizedSkyView(unittest.TestCase):
     pass
