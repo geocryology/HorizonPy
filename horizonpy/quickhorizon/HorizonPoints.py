ef6f0d563522ea6e0c320b3190c24bb01c099976
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2020-12-07 14:17:34 -0500

    create horizon points class

---
+++ horizonpy/quickhorizon/HorizonPoints.py
@@ -0,0 +1,32 @@
+class HorizonPoints:
+
+    def __init__(self):
+        self.dots = list()
+    
+    def import_horizon_csv():
+        pass
+
+    def import_geotop_csv():
+        pass
+
+    def import_data(self, data_type="horizon")
+        pass
+
+    def __get_import_method(self, data_type):
+        pass
+
+    def export_to_geotop():
+        pass
+
+    def export_to_horizon_csv(self):
+        pass
+
+    def delete_all(self):
+        pass
+
+    def add_dot(self):
+        pass
+
+    def get_dots(self):
+        pass
+
