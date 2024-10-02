bc0f357c52859f71a8cd93b8d6269dd07c9b52e9
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2024-10-02 11:05:56 -0600

    add debug mode

--- horizonpy/quickhorizon/main.py
+++ horizonpy/quickhorizon/main.py
@@ -1,5 +1,6 @@
 import logging
 import pkg_resources
+import argparse
 
 from os import path
 
@@ -13,12 +14,24 @@ except ImportError:
 
 
 def main():
+    parser = argparse.ArgumentParser(
+                    prog='Quickhorizon',
+                    description='Digitize horizon lines',
+                    epilog='Type "quickhorizon" at the command line to launch the app')
+    parser.add_argument("--debug", help="turn debug logging on", action='store_true')
+    args = parser.parse_known_args()[0]
+
     root = tk.Tk()
     root.title("QuickHorizon")
     icon = pkg_resources.resource_filename("horizonpy", path.join("quickhorizon", "assets", "QH.ico"))
     root.iconbitmap(icon)
 
-    logging.basicConfig(level=logging.DEBUG,
+    if args.debug:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+
+    logging.basicConfig(level=loglevel,
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S')
 
